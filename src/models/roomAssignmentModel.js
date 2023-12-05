4658a7b35180f4d076a9536e12861b07c8efe175
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-12-06 03:07:29 +1100

    feat: Create room assignment schema

---
+++ src/models/roomAssignmentModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const Occupant = require('./occupantModel');
+const Room = require('./roomModel');
+
+
+const roomAssignmentSchema = new Schema({
+    // Room reference
+    room: { 
+        type: mongoose.Types.ObjectId, 
+        ref: 'Room' 
+    }, 
+
+    // Occupant reference
+    occupant: { 
+        type: mongoose.Types.ObjectId, 
+        ref: 'Occupant' 
+    }, 
+
+    // Agreement reference
+
+    agreement: {
+        startDate: { 
+            type: Date, 
+            required: true 
+        },  
+        endDate: { 
+            type: Date,
+            required: true 
+        },  
+        rentInclusions: { 
+            type: [String], 
+            required: true 
+        },
+        rentalPayment: { 
+            type: Number, 
+            required: true 
+        },
+        rentalPaymentFrequency: { 
+            type: String, 
+            required: true 
+        },
+        securityDeposit: { 
+            type: Number, 
+            required: true 
+        },
+    },
+    rentalAgreementStatus: String
+});
+
+module.exports = mongoose.model('RoomAssignment', roomAssignmentSchema);
