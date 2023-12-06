d0ad3726cbe234076b08af4aa805c5c518f700f7
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-12-07 10:49:38 +1100

    feat: Create user Model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,57 +1,100 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
 
+const AddressSchema = new Schema({
+    street: String,
+    number: Number,
+    city: String,
+    postCode: String,
+    state: String
+});
 
 const UserSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    dob: { type: Date, required: true },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        match: /\S+@\S+\.\S+/ // simple regex to validate email format
     },
-    lastName: {
-        type: String,
-        required: true
-    },
-    dob: {
-        type: Date,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    phoneNumber: {
-        type: String,
-        required: false
-    },
-    address: { 
-        street: {
-            type: String,
-            required: false
-        },
-        number: {
-            type: Number,
-            required: false
-        },
-        city: {
-            type: String,
-            required: false
-        },
-        postCode: {
-            type: String,
-            required: false
-        },
-        state: {
-            type: String,
-            required: false
-        }
-    },
+    password: { type: String, required: true },
+    phoneNumber: String,
+    address: AddressSchema
+});
 
+UserSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+// const { Schema } = mongoose;
+// const bcrypt = require('bcrypt');
+
+
+// const UserSchema = new Schema({
+//     firstName: {
+//         type: String,
+//         required: true
+//     },
+//     lastName: {
+//         type: String,
+//         required: true
+//     },
+//     dob: {
+//         type: Date,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     },
+//     phoneNumber: {
+//         type: String,
+//         required: false
+//     },
+//     address: { 
+//         street: {
+//             type: String,
+//             required: false
+//         },
+//         number: {
+//             type: Number,
+//             required: false
+//         },
+//         city: {
+//             type: String,
+//             required: false
+//         },
+//         postCode: {
+//             type: String,
+//             required: false
+//         },
+//         state: {
+//             type: String,
+//             required: false
+//         }
+//     },
+
+// });
+
+// module.exports = mongoose.model('User', UserSchema);
