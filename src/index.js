dd9adebddb3c4c00bb64baee87a1ca553fe00e6b
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-12-06 21:13:27 +1100

    feat: Add PORT variable to make connection modular

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 // This file handles the boot-up of the server 
 
 require('dotenv').config();
-
 const { databaseConnect } = require('./config/database');
 const { app } = require('./server');
 
-
-app.listen(3000, async () => {
-	await databaseConnect();
-	console.log("Server running!");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+  await databaseConnect();
+  console.log(`Server is running on port ${PORT}`);
 });
