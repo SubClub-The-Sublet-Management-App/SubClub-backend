1299b57541668dd696807bb7eded7b9d06296192
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-12-07 14:43:17 +1100

    feat: Update user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -13,7 +13,7 @@ const AddressSchema = new Schema({
 const UserSchema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    dob: { type: Date, required: true },
+    dob: { type: Date, required: false },
     email: { 
         type: String, 
         required: true, 
@@ -39,62 +39,3 @@ module.exports = mongoose.model('User', UserSchema);
 
 
 
-
-
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
-// const bcrypt = require('bcrypt');
-
-
-// const UserSchema = new Schema({
-//     firstName: {
-//         type: String,
-//         required: true
-//     },
-//     lastName: {
-//         type: String,
-//         required: true
-//     },
-//     dob: {
-//         type: Date,
-//         required: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-//     phoneNumber: {
-//         type: String,
-//         required: false
-//     },
-//     address: { 
-//         street: {
-//             type: String,
-//             required: false
-//         },
-//         number: {
-//             type: Number,
-//             required: false
-//         },
-//         city: {
-//             type: String,
-//             required: false
-//         },
-//         postCode: {
-//             type: String,
-//             required: false
-//         },
-//         state: {
-//             type: String,
-//             required: false
-//         }
-//     },
-
-// });
-
-// module.exports = mongoose.model('User', UserSchema);
