7da8766d55ed5349e68f1555596d22feb3f898ac
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-12-07 18:13:27 +1100

    feat: Create authMiddleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/UserModel');
+require('dotenv').config();
+
+const authMiddleware = async (req, res, next) => {
+    // Get token from the header
+    const authHeader = req.header('Authorization');
+    if (!authHeader) {
+        return res.status(401).json({ message: 'No Authorization header, authorization denied' });
+    }
+    const token = authHeader.replace('Bearer ', '');
+
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        console.log(decoded);
+
+        // Find the user
+        const user = await User.findById(decoded.userId);
+        console.log(user);
+        console.log(decoded.id);
+        if (!user) {
+            return res.status(401).json({ message: 'User not found, authorization denied' });
+        }
+
+        // Attach the user to the request object
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Token is not valid, authorization denied' });
+    }
+};
+
+module.exports = authMiddleware;
