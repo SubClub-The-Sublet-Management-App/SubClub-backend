a199005a3e5895132e812d89afcc51152d6cbe86
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-12-13 22:07:20 +1100

    fix: index file setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // desc: This file handles the boot-up of the server 
 require('dotenv').config();
-const { dataBaseConnect } = require('./config/dataBase');
-const { app } = require('./server');
+import { dataBaseConnect } from './config/database'; 
+
+import { app } from './server';
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
